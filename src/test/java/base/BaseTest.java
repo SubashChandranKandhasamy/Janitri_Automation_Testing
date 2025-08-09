c76017e5a7e6d36f708ecd2a2f4b978091f475ad
SubashChandranKandhasamy <subashchandrankandhasamy@gmail.com>
2025-08-09 09:20:56 +0530

    Initial commit

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,29 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().deleteAllCookies(); // Ensure clean state
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://dev-dash.janitri.in");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
